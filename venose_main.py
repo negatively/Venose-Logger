48c575ea4c4f4f1b33f2b9049ce07d2ec35e4ce6
negatively <mileniandi39@gmail.com>
2022-04-25 13:08:59 +0700

    optimasi ukuran untuk layar raspberry 5 inch

--- venose_main.py
+++ venose_main.py
@@ -12,11 +12,10 @@ class RootGUI:
     def __init__(self):
         self.root = Tk()
         self.root.title("Serial Communication")
-        # self.root.state('zoomed')
+        self.root.geometry("800x480")
+        # self.root.attributes('-fullscreen', True)
+        self.root.option_add("*Font", "Arial 7")
         self.root.config(bg='#fff')
-
-class Graphics():
-    pass
 
 class ComGUI():
     def __init__(self, root, serial, data):
@@ -26,9 +25,9 @@ class ComGUI():
         self.data = data
         self.frame = LabelFrame(root, text="COM Manager", padx = 5, pady = 5, bg = '#fff')
         self.label_com = Label(
-            self.frame, text="Available Port : ", bg='#fff', width=15, anchor='w')
+            self.frame, text="Port : ", bg='#fff', width=8, anchor='w')
         self.label_bd = Label(
-            self.frame, text='Baud Rate : ', bg='#fff', width=15, anchor='w')
+            self.frame, text='Baud Rate : ', bg='#fff', width=8, anchor='w')
         self.com_option_menu()
         self.baud_option_menu()
         self.btn_refresh = Button(self.frame, text="Refresh", width=10,
@@ -36,7 +35,7 @@ class ComGUI():
         self.btn_connect = Button(self.frame, text="Connect",
                                   width=10, state="disabled",  command=self.serial_connect)
 
-        self.padx = 20
+        self.padx = 2
         self.pady = 12
         self.publish()
         
@@ -125,24 +124,24 @@ class ConnGUI():
         self.serial = serial
         self.data = data
 
-        self.frame = LabelFrame(root, text="Serial Read Manager", padx = 5, pady=5, bg='#fff',
+        self.frame = LabelFrame(root, text="Serial Read Manager", padx = 2, pady=5, bg='#fff',
                                 width=60)
 
         ########## Label ##########        
         self.flush_label = Label(
-            self.frame, text="Flush Time : ", bg='#fff', width=15, anchor='w')
+            self.frame, text="Flush Time : ", bg='#fff', width=10, anchor='w')
         self.sample_label = Label(
-            self.frame, text="Sample Time : ", bg='#fff', width=15, anchor='w')
+            self.frame, text="Sample Time : ", bg='#fff', width=10, anchor='w')
         self.purge_label = Label(
-            self.frame, text="Purge Time : ", bg='#fff', width=15, anchor='w')
+            self.frame, text="Purge Time : ", bg='#fff', width=10, anchor='w')
 
         ########## Button ##########        
         self.btn_start_stream = Button(
-            self.frame, text="Start", state='disabled', width=15, command=self.start_stream)
+            self.frame, text="Start", state='disabled', width=12, command=self.start_stream)
         self.btn_stop_stream = Button(
-            self.frame, text="Stop", state='disabled', width=15, command=self.stop_stream)
+            self.frame, text="Stop", state='disabled', width=12, command=self.stop_stream)
         self.btn_save_stream = Button(
-            self.frame, text="Save To CSV", state='disabled', width=15, command=self.save_stream)
+            self.frame, text="Save To CSV", state='disabled', width=12, command=self.save_stream)
 
         ########## Call Option Menu ########## 
         self.flush_option_menu()
@@ -150,37 +149,35 @@ class ConnGUI():
         self.purge_option_menu() 
 
         ########## Display For Temperature ########## 
-        self.graph = Graphics()
         # Static Part #        
-        self.graph.canvas = Canvas(self.frame, width = 100, height=100, bg='#fff', highlightthickness=0)  
-        self.graph.canvas.create_rectangle(75,75,25,25,outline = '#f11', fill='#fff', width=2)
-        self.graph.canvas.create_text(65, 10, anchor=E, text='Temp')
+        self.canvas = Canvas(self.frame, width = 100, height=100, bg='#fff', highlightthickness=0)  
+        self.canvas.create_rectangle(75,75,25,25,outline = '#f11', fill='#fff', width=2)
+        self.canvas.create_text(65, 10, anchor=E, text='Temp')
         # Dynamic Part #
-        self.graph.text = self.graph.canvas.create_text(64, 60, anchor=E, text='---', font=('Arial 10 bold'))
+        self.text = self.canvas.create_text(64, 60, anchor=E, text='---', font=('Arial 10 bold'))
         # Static Part #
-        self.graph.canvas.create_text(55, 65, anchor=E, text='C', font=('Arial 10 bold'))
+        self.canvas.create_text(55, 65, anchor=E, text='C', font=('Arial 10 bold'))
 
         self.serial.t4 = threading.Thread(
             target=self.serial.SerialTemp, args=(self,), daemon = True
         )
-        self.serial.t4.start()      
-        
+        self.serial.t4.start()
 
         ##########################################
         
-        self.separator = ttk.Separator(self.frame, orient='vertical')
+        # self.separator = ttk.Separator(self.frame, orient='vertical')
         self.separator2 = ttk.Separator(self.frame, orient='vertical')
-        self.padx = 20
+        self.padx = 2
         self.pady = 3
         self.chartMain = PlotData(self.root, self.serial, self.data)
         self.ConnGUIOpen()
 
     def GraphCtrl(self):
-        self.graph.canvas.itemconfig(self.graph.text, text=f"{self.data.StrMsg}")
+        self.canvas.itemconfig(self.text, text=f"{self.data.StrMsg}")
     
     def ConnGUIOpen(self):
         
-        self.frame.grid(row=0, column=4, rowspan=3, columnspan=5, padx=5, pady=5)
+        self.frame.grid(row=0, column=4, rowspan=3, columnspan=5, padx=2, pady=5)
 
         self.flush_label.grid(column=1, row=1)
         self.drop_flush.grid(column=2, row=1, padx=self.padx, pady=self.pady)
@@ -191,16 +188,15 @@ class ConnGUI():
         self.purge_label.grid(column=1, row=3)
         self.drop_purge.grid(column=2, row=3, padx=self.padx, pady=self.pady)
 
-        self.separator.place(relx=0.5, rely=0, relwidth=0.001, relheight=1)
-        self.separator2.place(relx=0.81, rely=0, relwidth=0.001, relheight=1)
+        # self.separator.place(relx=0.452, rely=0, relwidth=0.001, relheight=1)
+        self.separator2.place(relx=0.74, rely=0, relwidth=0.001, relheight=1)
 
         self.btn_start_stream.grid(column=3, row=1, padx=self.padx)
         self.btn_stop_stream.grid(column=3, row=2, padx=self.padx)
         self.btn_save_stream.grid(column=3, row=3, padx=self.padx)        
 
-        self.graph.canvas.grid(column = 4, row=1, rowspan = 3)
+        self.canvas.grid(column = 4, row=1, rowspan = 3)
         self.chartMain.OpenPlot()
-
 
     def ConnGUIClose(self):
         self.chartMain.ClosePlot()
@@ -208,14 +204,13 @@ class ConnGUI():
             widget.destroy()
         self.frame.destroy()
 
-
     def flush_option_menu(self):
         t_flush = ["-", '10', '20', '30', '40', '50', '60']
         self.clicked_flush = StringVar()
         self.clicked_flush.set(t_flush[0])
         self.drop_flush = OptionMenu(
             self.frame, self.clicked_flush, *t_flush, command=self.start_ctrl)
-        self.drop_flush.config(width=10)
+        self.drop_flush.config(width=8)
     
     def sample_option_menu(self):
         t_sample = ["-", '15','30', '45', '60', '75', '90', '105', '120']
@@ -223,7 +218,7 @@ class ConnGUI():
         self.clicked_sample.set(t_sample[0])
         self.drop_sample = OptionMenu(
             self.frame, self.clicked_sample, *t_sample, command=self.start_ctrl)
-        self.drop_sample.config(width=10)
+        self.drop_sample.config(width=8)
 
     def purge_option_menu(self):
         t_purge = ["-", '15', '30', '45', '60', '75', '90', '105', '120']
@@ -231,10 +226,9 @@ class ConnGUI():
         self.clicked_purge.set(t_purge[0])
         self.drop_purge = OptionMenu(
             self.frame, self.clicked_purge, *t_purge, command=self.start_ctrl)
-        self.drop_purge.config(width=10)
+        self.drop_purge.config(width=8)
     
-    def start_ctrl(self, widget):       
-        
+    def start_ctrl(self, widget):
         if "-" in self.clicked_flush.get() or "-" in self.clicked_sample.get() or "-" in self.clicked_purge.get():
             self.btn_start_stream["state"] = "disabled"
         else:
@@ -244,16 +238,14 @@ class ConnGUI():
         self.serial.threading_temp = False
         self.data.ClearData()
         # Menambahkan figure plot
-        self.serial.ser.write(bytes('H', 'UTF-8'))
+        self.serial.ser.write(bytes('S', 'UTF-8'))
 
         # Mengatur keadaan state button stop dan start
         self.btn_stop_stream["state"] = "active"
         self.btn_start_stream["state"] = "disabled"
         self.btn_save_stream["state"] = "disabled"
-
         self.chartMain.chart.get_tk_widget().grid()
 
-        self.root.geometry("1200x800")
         if len(self.data.xData) == 0:
             self.refTime = time.perf_counter()
         else:
@@ -276,7 +268,6 @@ class ConnGUI():
         self.btn_save_stream["state"] = "active"
 
         # self.chartMain.chart.get_tk_widget().grid_remove()
-
         self.serial.threading = False
     
     def save_stream(self):
@@ -289,8 +280,8 @@ class ConnGUI():
         self.chartMain.ax.grid(color='b', linestyle='-', linewidth=0.2)
         labels = ['TGS-2600', 'TGS-2611', 'MQ-3', 'TGS-822', 'MQ-9', 'MQ-135']
         self.chartMain.ax.legend(labels, prop={'size':6})
-        self.chartMain.ax.set_ylabel('Data Sensor (mV)')
-        self.chartMain.ax.set_xlabel('Time (s)')
+        self.chartMain.ax.set_ylabel('Data Sensor (mV)', fontsize=5)
+        self.chartMain.ax.set_xlabel('Time (s)', fontsize=5)
         self.chartMain.fig.canvas.draw()
         if self.serial.threading:
             self.root.after(250, self.UpdateChart)
@@ -312,8 +303,7 @@ class ConnGUI():
             self.stop_stream() 
     
         if self.serial.threading:
-            self.root.after(250, self.UpdateTime)  
-        
+            self.root.after(250, self.UpdateTime)        
 
 
 class PlotData():
@@ -321,12 +311,12 @@ class PlotData():
         self.root = root
         self.serial = serial
         self.data = data
-        self.frame = LabelFrame(self.root, text="Plotting Data", padx = 5, pady = 5, bg='#fff')
+        self.frame = LabelFrame(root, text="Plotting Data", padx = 2, pady = 2, bg='#fff', width = 60)
         self.CreatFig()
     
     def OpenPlot(self):        
-        self.frame.grid(padx=5, columnspan = 9,sticky = NW)
-        self.chart.get_tk_widget().grid(column=0, row=10, columnspan=20, rowspan=20)
+        self.frame.grid(padx=2, columnspan = 7,sticky = NW)
+        self.chart.get_tk_widget().grid(column=0, row=4, columnspan=1, rowspan=1)
         self.chart.get_tk_widget().grid_remove()
 
     def ClosePlot(self):
@@ -336,10 +326,12 @@ class PlotData():
     
     def CreatFig(self):
         ########## Plot Set Up ##########         
-        self.fig = plt.Figure(figsize=(7,5), dpi=100)
+        self.fig = plt.Figure(figsize=(6,2.5), dpi=100)
         self.ax = self.fig.add_subplot(111)
         self.chart = FigureCanvasTkAgg(self.fig, master=self.frame)
-        self.chart.get_tk_widget().grid(column=0, row=10, columnspan=20, rowspan=20)
+        self.chart.get_tk_widget().grid(column=0, row=4, columnspan=1, rowspan=1)
+        plt.rc('xtick', labelsize=5)
+        plt.rc('ytick', labelsize=5) 
     
 
 
